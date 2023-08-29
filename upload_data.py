d17cb649d7c3a90c6e3e8df30b1c91f5ec416151
Daniel Genkins <64920363+d-genk@users.noreply.github.com>
2023-08-29 15:53:17 -0500

    fixed encoding bug

--- upload_data.py
+++ upload_data.py
@@ -11,12 +11,11 @@ for folder, subfolders, files in os.walk("E:\\ssda-htr-data\\aligned"):
                 img_data = f.read()
             headers = {"Content-Type":"image/jpeg"}
             requests.put("https://zoqdygikb2.execute-api.us-east-1.amazonaws.com/v1/ssda-htr-training/" + key, data=img_data, headers=headers)
-        elif 'txt' in file:
+        if 'txt' in file:
             line_id = 0
             with open(os.path.join(folder, file), 'r', encoding='utf-8') as f:
                 for line in f:
                     line_id += 1
                     key = base_id + '0' * (2 - len(str(line_id))) + str(line_id) + '.txt'
                     headers = {"Content-Type":"text/plain"}
-                    requests.put("https://zoqdygikb2.execute-api.us-east-1.amazonaws.com/v1/ssda-htr-training/" + key, data=line.replace('\n', ''))  
-        
+                    requests.put("https://zoqdygikb2.execute-api.us-east-1.amazonaws.com/v1/ssda-htr-training/" + key, data=line.replace('\n', '').encode('utf-8'))
